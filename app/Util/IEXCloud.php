e278adabad9b416310613e9c04cad5f1abeb4e1e
Thomas Grauer <tgrauer1@me.com>
2020-08-02 22:57:57 -0400

    Adding local vars

--- app/Util/IEXCloud.php
+++ app/Util/IEXCloud.php
@@ -6,16 +6,40 @@ use GuzzleHttp\Client;
 class IEXCloud
 {
 	protected $client;
+	protected $PRODUCTION_URL = 'https://cloud.iexapis.com/';
+	protected $SANDBOX_URL = 'https://sandbox.iexapis.com/';
+	protected $sandbox = FALSE;
+	protected $version = 'stable';
+    protected $baseURL;
 
-	public function __construct(Client $client)
+	public function __construct(bool $sandbox = FALSE)
 	{
-		$this->client = $client;
+		$this->sandbox = $sandbox;
+		$this->setClient();
+		$this->setBaseURL();
+		// $this->client = $client;
 	}
+
+	protected function setClient() 
+	{
+        $this->client = new Client( [
+            'base_uri' => $this->baseURL,
+        ] );
+    }
+
+    protected function setBaseURL()
+    {
+    	if($this->sandbox){
+    		$this->baseURL = $this->SANDBOX_URL;
+    	}else{
+    		$this->baseURL = $this->PRODUCTION_URL;
+    	}
+    }
 
 	public function sendRequest($uri, $extra_params=null)
 	{
 		$extra_params = !empty($extra_params) ? '&'.$extra_params : '';
-		$uri= env('IEXCLOUD_BASE_URL').$uri.'?token='.env('IEXCLOUD_API_KEY') . $extra_params;
+		$uri = env('IEXCLOUD_BASE_URL').$uri.'?token='.env('IEXCLOUD_API_KEY') . $extra_params;
 		// return $uri;
 		try {
 			$response = $this->client->request('GET', $uri);
@@ -34,5 +58,4 @@ class IEXCloud
 		
 		return [];
 	}
-
 }
